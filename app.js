6fd020c3d98c72154cc74c226dfbffe5f4c9ce4e
thomas-jiitak <thomasjoseph@jiitak.com>
2025-01-27 12:42:15 +0530

    test phase

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get('/',(req,res) => {
     res.send('Hello World! from Node.Js + Changes ');
 });
 app.listen('8000','0.0.0.0',() =>{
-    console.log('listening to port 8k');
+    console.log('listening to port 8000 +');
 });
