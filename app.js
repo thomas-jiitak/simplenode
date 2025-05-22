056718e9694ca7ee05915ad26a8c67109ff57d3e
thomas-jiitak <thomasjoseph@jiitak.com>
2025-05-22 17:07:15 +0530

    key

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 'use strict';
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send('Hello World! from Node.Js + Changes ');
+// Load environment variables from .env file
+dotenv.config();
+
+const KEY = process.env.KEY;
+
+app.get('/', (req, res) => {
+    res.send(`Hello World! from Node.Js + Changes ${KEY}`);
 });
-app.listen('80','0.0.0.0',() =>{
-    console.log('listening to port 8000 +');
+
+app.listen(80, '0.0.0.0', () => {
+    console.log('listening on port 80');
 });
